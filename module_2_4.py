b53c932af97ccede8a9cd925f0515e852fdd9dc0
Nikitus200 <sm.nikita2007@mail.ru>
2024-06-25 20:01:44 +0500

    Модуль 2

---
+++ module_2_4.py
@@ -0,0 +1,16 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for i in numbers:
+    if i == 1:
+        continue
+    count = 0
+    for j in range(1, i + 1):
+        if i % j == 0:
+            count += 1
+    if count > 2:
+        not_primes.append(i)
+    else:
+        primes.append(i)
+print(f"Primes: {primes}")
+print(f"Not Primes: {not_primes}")
